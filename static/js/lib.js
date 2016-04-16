251bf87cd7872967b663f69493ac22f34123c438
Nick-Omen <nikita.omen666@gmail.com>
2016-04-16 16:50:39 +0600

    Added modal with works

--- static/js/lib.js
+++ static/js/lib.js
@@ -64,8 +64,29 @@ var works = [
 var myLib= function() {
     'use strict';
 
+    // Reference to the class
     var lib = this;
+    this.defaultDelay = 400; // ms
 
+    /**
+     * Get work by ID from portfolio array
+     * @param id
+     * @returns {*}
+     */
+    this._getWorkById = function(id) {
+        var work = {};
+        for (var i in works) {
+            if(works.hasOwnProperty(i)) {
+                if(works[i].id == id) return work = works[i];
+            }
+        }
+        return work;
+    };
+
+    /**
+     * Create a DOM of a work item
+     * @returns {*|jQuery}
+     */
     this._workItem = function() {
         return $('<div class="item col-lg-4 col-sm-6">').append($('<div class="portfolio-box">')
             .append($('<img class="img-responsive" src="">'))
@@ -77,12 +98,32 @@ var myLib= function() {
                 )));
     };
 
+    /**
+     * Create a DOM of a work detail
+     * @param id
+     * @returns {*|jQuery}
+     */
+    this._workDetail = function(id) {
+        var work = lib._getWorkById(id);
+        return $('<div class="row">')
+            .append($('<div class="col-md-6">')
+                .append($('<div class="title">').append($('<a href="' + work.siteUrl + '" rel="nofollow">').text(work.siteName)))
+                .append($('<div class="work-type">').text(work.workType))
+                .append($('<div class="technologies">').text(work.technologies))
+                .append($('<div class="description">').text(work.description)))
+            .append($('<div class="col-md-6">')
+                .append('<img class="img-responsive" src="' + work.imageUrl + '" alt="' + work.siteName + '">'));
+    };
+
+    /**
+     * Create DOM with all works and insert it to document
+     */
     this._renderWorks = function() {
         var $workContainer = $('#work-container');
         for (var i in works) {
             if(works.hasOwnProperty(i)) {
                 var $item = lib._workItem();
-                $item.data('id', works[i].id);
+                $item.attr('id', works[i].id);
                 $item.find('img').attr('src', works[i].imageUrl).attr('alt', works[i].siteName);
                 $item.find('.project-category').text(works[i].workType);
                 $item.find('.project-name').text(works[i].siteName);
@@ -90,8 +131,45 @@ var myLib= function() {
                 $workContainer.append($item);
             }
         }
+        lib._workClickHandler();
     };
 
+    /**
+     * Handle click on work and open modal with work details
+     */
+    this._workClickHandler = function() {
+        var $works = $('#work-container').find('.item');
+
+        $works.on('click touchend', function() {
+            var $modal = lib._createModal();
+            var $modalContent = $modal.find('.modal-content');
+            var $workDetails = lib._workDetail($(this).attr('id'));
+
+            $modalContent.append($workDetails);
+            $(this).closest('section').append($modal);
+
+            $modalContent.find('.close').on('click touchend', function() {
+                $modal.addClass('close');
+                setTimeout(function(){
+                    $modal.remove();
+                }, 800)
+            });
+        });
+    };
+
+    /**
+     * Create DOM of a modal window
+     * @returns {*|jQuery}
+     */
+    this._createModal = function() {
+        return $('<div class="modal-mask">')
+            .append($('<div class="modal-content container">')
+                .append($('<div class="close">')));
+    };
+
+    /**
+     * Expand main menu when page scroll
+     */
     this._expandMenu = function() {
         $(window).on('scroll', function(event) {
             event.stopPropagation();
@@ -104,6 +182,9 @@ var myLib= function() {
         });
     };
 
+    /**
+     * Sly scroll to anchors with the page-scroll class
+     */
     this._slyScroll = function() {
         var $buttons = $('.page-scroll');
 
@@ -114,10 +195,13 @@ var myLib= function() {
 
             $buttons.removeClass('active');
             $el.addClass('active');
-            $('html, body').animate({scrollTop: $(scrollTo).offset().top - $('#mainNav').height()}, 500)
+            $('html, body').animate({scrollTop: $(scrollTo).offset().top - $('#mainNav').height()}, 400)
         });
     };
 
+    /**
+     *
+     */
     this._buttonsHandler = function() {
         var $buttons = $('.page-scroll');
 
@@ -131,9 +215,13 @@ var myLib= function() {
         });
     };
 
+    /**
+     * Handler to show dropdown with my own info
+     */
     this._showPersonalDataHandler = function() {
         var $menu = $('#mainNav');
 
+        // Add the About me link to menu if block is opened
         var addMenuItem = function() {
             var $nav = $menu.find('ul.nav');
             if($nav.find('.nav-person').length != 0) return;
@@ -146,22 +234,22 @@ var myLib= function() {
             lib._slyScroll();
         };
 
+        // Scroll to my personal info
+        var scrollToPersonalInfo = function($selector) {
+            $('html, body').animate({scrollTop: $selector.offset().top - $menu.height()}, lib.defaultDelay);
+            $selector.delay(lib.defaultDelay).animate({height: $selector.children().height() + 60}, lib.defaultDelay);
+        };
+
         if(window.location.hash && window.location.hash == '#person') {
             var $person = $('#person');
+            scrollToPersonalInfo($person);
             addMenuItem();
-            $('html, body').animate({scrollTop: $person.offset().top - $menu.height()}, 800);
-            $person.delay(400).animate({height: $person.children().height() + 60}, 400);
         }
 
         $('#person-handler').on('click touchend', function() {
-
-            var $el = $(this);
-            var $person = $($el.attr('href'));
+            var $person = $($(this).attr('href'));
+            scrollToPersonalInfo($person);
             addMenuItem();
-
-            $('html, body').animate({scrollTop: $person.offset().top - $menu.height()}, 300);
-            $person.delay(600).animate({height: $person.children().height() + 60}, 400);
-
         });
     };
 
