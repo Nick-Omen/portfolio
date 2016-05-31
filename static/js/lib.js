3683207908d565c8780c3a771a0b2f88b83fbee8
Nick-Omen <nikita.omen666@gmail.com>
2016-05-31 20:15:29 +0600

    added portfolio text, images

--- static/js/lib.js
+++ static/js/lib.js
@@ -1,63 +1,75 @@
 var works = [
     {
         id: 1,
-        imageUrl: 'img/portfolio/1.jpg',
+        imageUrl: '/img/portfolio/cn.jpg',
+        bigImageUrl: '/img/portfolio/cn_big.jpg',
         workType: 'Frontend, Backend',
         siteName: 'Cyber Namba',
         siteUrl: 'http://cybernamba.com/',
-        description: 'Big description here',
-        shortDescription: 'short description here',
-        technologies: 'tech i use'
+        description: 'The cyber platform created to organize cyber-sport competitions.',
+        shortDescription: 'The cyber platform created to organize cyber-sport competitions.',
+        languages: 'Python, HTML, CSS, JavaScript',
+        technologies: 'Django, Bootstrap3, jQuery, AJAX'
     },
     {
         id: 2,
-        imageUrl: 'img/portfolio/4.jpg',
+        imageUrl: '/img/portfolio/atletic.jpg',
+        bigImageUrl: '/img/portfolio/atletic_big.jpg',
         workType: 'Frontend, Backend',
         siteName: 'Athletic',
         siteUrl: 'http://atletic.kz/',
-        description: 'Big description here',
-        shortDescription: 'short description here',
-        technologies: 'tech i use'
+        description: 'Site created for BodyBuilding event in Almaty, Kazakhstan.',
+        shortDescription: 'The site for BodyBuilding event in Almaty, Kazakhstan.',
+        languages: 'PHP, HTML, CSS, JavaScript',
+        technologies: 'WordPress, Bootstrap3, jQuery, AJAX, GoogleMap, Slick Carousel'
     },
     {
         id: 3,
-        imageUrl: 'img/portfolio/5.jpg',
+        imageUrl: '/img/portfolio/express.jpg',
+        bigImageUrl: '/img/portfolio/express_big.jpg',
         workType: 'Frontend, Backend',
         siteName: 'Namba Express',
         siteUrl: 'http://nambaexpress.kg/',
-        description: 'Big description here',
-        shortDescription: 'short description here',
-        technologies: 'tech i use'
+        description: 'Landing page with contact form and blog for NambaExpress service.',
+        shortDescription: 'Landing page with contact form and blog for NambaExpress service.',
+        languages: 'PHP, HTML, CSS, JavaScript',
+        technologies: 'WordPress, Bootstrap3, jQuery, AJAX'
     },
     {
         id: 4,
-        imageUrl: 'img/portfolio/6.jpg',
+        imageUrl: '/img/portfolio/gbf.jpg',
+        bigImageUrl: '/img/portfolio/gbf_big.jpg',
         workType: 'Frontend, Backend',
         siteName: 'Global Business Forum',
         siteUrl: 'http://gbf.adline.kz/',
-        description: 'Big description here',
-        shortDescription: 'short description here',
-        technologies: 'tech i use'
+        description: 'Landing page with booking system for Global Business Forum in Almaty, Kazakhstan.',
+        shortDescription: 'Landing page with booking system for Global Business Forum in Almaty, Kazakhstan.',
+        languages: 'PHP, HTML, CSS, JavaScript',
+        technologies: 'WordPress, Bootstrap3, jQuery'
     },
     {
         id: 5,
-        imageUrl: 'img/portfolio/6.jpg',
+        imageUrl: '/img/portfolio/dobro.jpg',
+        bigImageUrl: '/img/portfolio/dobro_big.jpg',
         workType: 'Frontend, Backend',
         siteName: 'Charity Award Site',
         siteUrl: 'http://delaidobro.tv7.kz/',
-        description: 'Big description here',
-        shortDescription: 'short description here',
-        technologies: 'tech i use'
+        description: 'Platform which allows to upload photos and videos people can voute for. The platform include auto-grabbing social networks(facebook, vk, instagram) by tag and create new articles.',
+        shortDescription: 'Platform which allows to upload photos and videos people can voute for. The platform include auto-grabbing social networks(facebook, vk, instagram) by tag and create new articles.',
+        languages: 'Python, HTML, CSS, JavaScript',
+        technologies: 'Django, Bootstrap3, jQuery, AJAX, Scrapy'
     },
     {
         id: 6,
-        imageUrl: 'img/portfolio/6.jpg',
+        imageUrl: '/img/portfolio/key.jpg',
+        bigImageUrl: '/img/portfolio/key_big.jpg',
         workType: 'Frontend, Backend',
         siteName: 'Official Mul-T-Lock Agent',
         siteUrl: 'http://vzlomunet.kz/',
-        description: 'Big description here',
-        shortDescription: 'short description here',
-        technologies: 'tech i use'
+        description: 'Landing page with contact form for official representative Mul-T-Lock Agency.',
+        shortDescription: 'Landing page with contact form for official representative Mul-T-Lock Agency.',
+        languages: 'PHP, HTML, CSS, JavaScript',
+        technologies: 'WordPress, Bootstrap3, jQuery, AJAX'
     }
 ];
 
@@ -66,7 +78,7 @@ var myLib= function() {
 
     // Reference to the class
     var lib = this;
-    this.defaultDelay = 400; // ms
+    this.defaultDelay = 300; // ms
 
     /**
      * Get work by ID from portfolio array
@@ -105,14 +117,14 @@ var myLib= function() {
      */
     this._workDetail = function(id) {
         var work = lib._getWorkById(id);
-        return $('<div class="row">')
+        return $('<div class="row work-details">')
             .append($('<div class="col-md-6">')
-                .append($('<div class="title">').append($('<a href="' + work.siteUrl + '" rel="nofollow">').text(work.siteName)))
+                .append($('<div class="title">').append($('<a target="_blank" href="' + work.siteUrl + '" rel="nofollow">').text(work.siteName)))
+                .append($('<div class="description">').text(work.description))
                 .append($('<div class="work-type">').text(work.workType))
-                .append($('<div class="technologies">').text(work.technologies))
-                .append($('<div class="description">').text(work.description)))
-            .append($('<div class="col-md-6">')
-                .append('<img class="img-responsive" src="' + work.imageUrl + '" alt="' + work.siteName + '">'));
+                .append($('<div class="technologies">').text(work.technologies)))
+            .append($('<div class="col-md-6 image-container">')
+                .append('<img class="img-responsive" src="' + work.bigImageUrl + '" alt="' + work.siteName + '">'));
     };
 
     /**
@@ -124,10 +136,10 @@ var myLib= function() {
             if(works.hasOwnProperty(i)) {
                 var $item = lib._workItem();
                 $item.attr('id', works[i].id);
-                $item.find('img').attr('src', works[i].imageUrl).attr('alt', works[i].siteName);
+                $item.find('img').css('background-image', 'url(' + works[i].imageUrl + ')').attr('src', '/img/blank.png').attr('alt', works[i].siteName);
                 $item.find('.project-category').text(works[i].workType);
                 $item.find('.project-name').text(works[i].siteName);
-                $item.find('.project-short').text('< ' + works[i].shortDescription + ' >');
+                $item.find('.project-short').text('< ' + works[i].languages + ' >');
                 $workContainer.append($item);
             }
         }
@@ -140,40 +152,34 @@ var myLib= function() {
     this._workClickHandler = function() {
         var $works = $('#work-container').find('.item');
 
-        $works.on('click touchend', function() {
-            var $modal = lib._createModal();
-            var $modalContent = $modal.find('.modal-content');
+        $works.on('click', function() {
+            var $modal = $('#work-modal');
+            var $modalContent = $modal.find('.modal-body');
             var $workDetails = lib._workDetail($(this).attr('id'));
+            $modalContent.html($workDetails);
 
-            $modalContent.append($workDetails);
-            $(this).closest('section').append($modal);
+            var pageCenter = {};
+            var centerCoordinates = {};
+            pageCenter.x = Math.round($(window).width()/2);
+            pageCenter.y = Math.round($(window).height()/2);
+            centerCoordinates.x = Math.round($(this).offset().left - $(this).width()/2);
+            centerCoordinates.y = Math.round($(this).offset().top - $(this).height()/2);
+
+            $modal.removeClass('close');
 
             var closeModal = function() {
                 $modal.addClass('close');
-                setTimeout(function(){
-                    $modal.remove();
-                }, lib.defaultDelay)
             };
 
-            $modalContent.find('.close').on('click touchend', function() {
+            $modal.find('.close').on('click', function() {
                 closeModal();
             });
-            $(document).on('click touchstart', function(e){
+            $(document).on('click', function(e){
                 if($(e.target).is($('.modal-mask'))){
                     closeModal();
                 }
             });
         });
-    };
-
-    /**
-     * Create DOM of a modal window
-     * @returns {*|jQuery}
-     */
-    this._createModal = function() {
-        return $('<div class="modal-mask">')
-            .append($('<div class="modal-content container">')
-                .append($('<div class="close">')));
     };
 
     /**
